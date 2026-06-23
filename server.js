011c6173b016a7da6ed30a772fa7bd3b2b8e90e0
itzmani-cmd <itzmani-cmd>
2026-06-23 14:05:50 +0530

    change the rate limit 500 to 5k

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 5000, // limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again later.',
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
